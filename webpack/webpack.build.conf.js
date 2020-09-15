0a51ee66fd5d49b66a98eae8da07f2ee839a5c9c
Alex K <contact.alexsophia@gmail.com>
2020-09-15 11:41:05 +0300

    Set up Github pages.

--- webpack/webpack.build.conf.js
+++ webpack/webpack.build.conf.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
